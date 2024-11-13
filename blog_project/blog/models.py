757157fb4ff5ccde0aa18e29411e0d0a5e6eb857
John3210of <john3210of@gmail.com>
2024-11-13 14:03:14 +0900

    feature board model

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
