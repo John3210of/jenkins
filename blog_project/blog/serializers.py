1315bc06fa84177def835f65f200a06f711fa1e9
John3210of <john3210of@gmail.com>
2024-11-13 14:46:08 +0900

    timezone sync

--- blog_project/blog/serializers.py
+++ blog_project/blog/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Board
-
 class BoardSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S") 
     class Meta:
         model = Board
         fields = '__all__'  # 모든 필드를 포함
