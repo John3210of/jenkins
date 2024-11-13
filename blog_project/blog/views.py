ed34936d15f250e165a7498e1d70ce06696ff54a
John3210of <john3210of@gmail.com>
2024-11-13 14:38:44 +0900

    board can list

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,7 +1,19 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import viewsets
-from blog.serializers import BoardSerializer
 from .models import Board
+from .serializers import BoardSerializer
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    def create(self, request, *args, **kwargs):
+        if isinstance(request.data, list):
+            serializer = self.get_serializer(data=request.data, many=True)
+        else:
+            serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        if isinstance(request.data, list):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
